56433a3d20d3990f80d74e5d9806e1020a61cdac
Xander <s10267815@connect.np.edu.sg>
2024-11-26 15:36:21 +0800

    day 3 (2)

--- code.js
+++ code.js
@@ -1,23 +1,29 @@
-let slideIndex = 0;
-showSlides();
 
-function showSlides() {
-    let slides = document.querySelectorAll(".slide");
-    
-    // Hide all slides
-    slides.forEach(slide => {
-        slide.style.display = "none";
-    });
+let slideIndex = 1;
+showSlides(slideIndex);
 
-    // Increment slides to show each slide
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1; // resets to the first sldie
-    }
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-    // displays the current slide
-    slides[slideIndex - 1].style.display = "block";
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-    // change slide every 3s
-    setTimeout(showSlides, 3000);
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
